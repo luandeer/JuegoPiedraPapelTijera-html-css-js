0dd1ccffe3cf8e189de8822676d4553095b9e0af
alexander <alexanderperezreginaldo@gmail.com>
2022-12-21 19:19:09 -0500

    agregando un mapa de eleccion de rival

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -20,6 +20,10 @@ const ataquesDelEnemigo = document.querySelector("#ataques-del-enemigo");
 
 const contenedorTarjetas = document.querySelector("#contenedorTarjetas");
 const contenedorAtaques = document.querySelector("#contenedorAtaques");
+
+const sectionVerMapa = document.querySelector("#ver-mapa");
+const mapa = document.querySelector("#mapa");
+
 //es el array donde se guardaran los objetos
 var pipati = [];
 
@@ -28,41 +32,98 @@ var inputMessi;
 var inputCristiano;
 var inputNeymar;
 var personajesJugador;
+var miJugador;
 var ataqueJugador;
 var ataqueEnemigo;
 var ataquesPipadi;
-var botones =[];
+var botones = [];
 var botonPiedra;
 var botonPapel;
 var botonTijera;
-
-
+var lienzo = mapa.getContext("2d");
+var intervalo;
+var mapaBackground = new Image();
+mapaBackground.src = "./assets/img/escenarioMapa.jpg";
 
 var vidasJugador = 3;
 var vidasEnemigo = 3;
 
 //esto es una clase que en un ejemplo real vendria a hacer como animales
 class Pipati {
-  constructor(nombre, foto, vida) {
+  constructor(nombre, foto, vida, fotoMapa, x = 10, y = 10) {
     //propiedades
     this.nombre = nombre;
     this.foto = foto;
     this.vida = vida;
     this.ataques = [];
+    this.x = x;
+    this.y = y;
+    this.ancho = 70;
+    this.alto = 70;
+    this.mapaFoto = new Image();
+    this.mapaFoto.src = fotoMapa;
+    this.velocidadx = 0;
+    this.velocidady = 0;
+  }
+  pintarJugador() {
+    lienzo.drawImage(this.mapaFoto, this.x, this.y, this.ancho, this.alto);
   }
 }
+
 //creando un objeto a partir de la clase creada, en un ejemplo real sera gato, perro, etc.
-var messi = new Pipati("Messi", "../src/assets/img/messims10.jpg", 5);
+var messi = new Pipati(
+  "Messi",
+  "../src/assets/img/messims10.jpg",
+  5,
+  "../src/assets/img/messi.jpg"
+);
 
 var cristiano = new Pipati(
   "Cristiano",
   "../src/assets/img/cristianocr7.jpg",
-  5
+  5,
+  "../src/assets/img/cristiano.jpg"
+);
+var neymar = new Pipati(
+  "Neymar",
+  " ../src/assets/img/neymar.jpg",
+  5,
+  "../src/assets/img/ney.jpg"
 );
 
-var neymar = new Pipati("Neymar", " ../src/assets/img/neymar.jpg", 5);
+var neymarEnemigo = new Pipati(
+  "Neymar",
+  " ../src/assets/img/neymar.jpg",
+  5,
+  "../src/assets/img/ney.jpg",
+  420,
+  110
+);
+
+var messiEnemigo = new Pipati(
+  "Messi",
+  "../src/assets/img/messims10.jpg",
+  5,
+  "../src/assets/img/messi.jpg",
+  420,
+  25
+);
+
+var cristianoEnemigo = new Pipati(
+  "Cristiano",
+  "../src/assets/img/cristianocr7.jpg",
+  5,
+  "../src/assets/img/cristiano.jpg",
+  420,
+  200
+);
 
 messi.ataques.push(
+  { nombre: "🥌", id: "boton-Piedra" },
+  { nombre: "🧻", id: "boton-Papel" },
+  { nombre: "✂", id: "boton-Tijera" }
+);
+messiEnemigo.ataques.push(
   { nombre: "🥌", id: "boton-Piedra" },
   { nombre: "🧻", id: "boton-Papel" },
   { nombre: "✂", id: "boton-Tijera" }
@@ -73,7 +134,18 @@ cristiano.ataques.push(
   { nombre: "🧻", id: "boton-Papel" },
   { nombre: "✂", id: "boton-Tijera" }
 );
+cristianoEnemigo.ataques.push(
+  { nombre: "🥌", id: "boton-Piedra" },
+  { nombre: "🧻", id: "boton-Papel" },
+  { nombre: "✂", id: "boton-Tijera" }
+);
+
 neymar.ataques.push(
+  { nombre: "🥌", id: "boton-Piedra" },
+  { nombre: "🧻", id: "boton-Papel" },
+  { nombre: "✂", id: "boton-Tijera" }
+);
+neymarEnemigo.ataques.push(
   { nombre: "🥌", id: "boton-Piedra" },
   { nombre: "🧻", id: "boton-Papel" },
   { nombre: "✂", id: "boton-Tijera" }
@@ -87,7 +159,8 @@ window.addEventListener(
   (iniciarJuego = () => {
     sectionSeleccionarAtaque.style.display = "none";
     sectionReiniciar.style.display = "none";
-
+    sectionVerMapa.style.display = "none";
+    
     //forEach por cada elemento se ejecuta una funcion en un array
     pipati.forEach((Pipati) => {
       //inserta parte del codigo de los personajes que aparecen en html
@@ -110,7 +183,9 @@ window.addEventListener(
       "click",
       (seleccionarPersonajeJugador = () => {
         sectionSeleccionarPersonaje.style.display = "none";
-        sectionSeleccionarAtaque.style.display = "flex";
+
+        sectionVerMapa.style.display = "flex";
+
         //checked(booleean) es para verificar si el usuario elegió correctamente.
         //inner.HTML va modificar el contenido de la etiqueta <span></span> en nuestro archivo html
         if (inputMessi.checked) {
@@ -130,11 +205,9 @@ window.addEventListener(
           }
         }
         extraerAtaques(personajesJugador);
-        seleccionarPersonajeEnemigo();
+        iniciarMapa();
       })
     );
-
-    
 
     botonReiniciar.addEventListener(
       "click",
@@ -161,14 +234,9 @@ function mostrarAtaques(ataques) {
     <button id=${ataque.id} class="boton-ataque BAtaque">${ataque.nombre}</button>`;
     contenedorAtaques.innerHTML += ataquesPipadi;
   });
-  botonPiedra = document.querySelector('#boton-Piedra');
-  botonPapel = document.querySelector('#boton-Papel');
-  botonTijera = document.querySelector('#boton-Tijera');
-
-  
-
-
-
+  botonPiedra = document.querySelector("#boton-Piedra");
+  botonPapel = document.querySelector("#boton-Papel");
+  botonTijera = document.querySelector("#boton-Tijera");
 
   botonPiedra.addEventListener(
     "click",
@@ -265,15 +333,116 @@ function crearMensajeFinal(resultadoFinal) {
   sectionReiniciar.style.display = "block";
 }
 
-
-
-function seleccionarPersonajeEnemigo() {
+function seleccionarPersonajeEnemigo(enemigo) {
   //lo que hacemos con la propiedad length es que te bote el numero de elementos del arreglo y se le resta -1 porque el arreglo inicia en el indice 0
-  var PersonajeAleatorio = aleatorio(0, pipati.length - 1);
+  //var PersonajeAleatorio = aleatorio(0, pipati.length - 1);
   //se tendra una sola fuente de verdad con esta logica
-  spanPersonajeEnemigo.innerHTML = pipati[PersonajeAleatorio].nombre;
+  //spanPersonajeEnemigo.innerHTML = pipati[PersonajeAleatorio].nombre;
+  spanPersonajeEnemigo.innerHTML = enemigo.nombre;
+  ataquesDelEnemigo = enemigo.ataques;
 }
 
 function aleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function pintarCanvas() {
+  miJugador.x = miJugador.x + miJugador.velocidadx;
+  miJugador.y = miJugador.y + miJugador.velocidady;
+  lienzo.clearRect(0, 0, mapa.width, mapa.height);
+  lienzo.drawImage(mapaBackground, 0, 0, mapa.width, mapa.height);
+
+  miJugador.pintarJugador();
+  messiEnemigo.pintarJugador();
+  cristianoEnemigo.pintarJugador();
+  neymarEnemigo.pintarJugador();
+  if (miJugador.velocidadx !== 0 || miJugador.velocidady !== 0) {
+    revisarColision(messiEnemigo);
+    revisarColision(cristianoEnemigo);
+    revisarColision(neymarEnemigo);
+  }
+}
+function moverDerecha() {
+  miJugador.velocidadx = 5;
+  pintarCanvas();
+}
+function moverIzquierda() {
+  miJugador.velocidadx = -5;
+  pintarCanvas();
+}
+function moverAbajo() {
+  miJugador.velocidady = 5;
+  pintarCanvas();
+}
+function moverArriba() {
+  miJugador.velocidady = -5;
+  pintarCanvas();
+}
+
+function iniciarMapa() {
+  mapa.width = 550;
+  mapa.height = 300;
+  miJugador = obtenerObjetoJugador();
+  intervalo = setInterval(pintarCanvas, 50);
+  window.addEventListener(
+    "keydown",
+    (sePresionoUnaTecla = (event) => {
+      switch (event.key) {
+        case "ArrowUp":
+          moverArriba();
+          break;
+        case "ArrowDown":
+          moverAbajo();
+          break;
+        case "ArrowLeft":
+          moverIzquierda();
+          break;
+        case "ArrowRight":
+          moverDerecha();
+          break;
+        default:
+          break;
+      }
+    })
+  );
+  window.addEventListener("keyup", detenerMovimiento);
+}
+function detenerMovimiento() {
+  miJugador.velocidadx = 0;
+  miJugador.velocidady = 0;
+}
+
+function obtenerObjetoJugador() {
+  for (let i = 0; i < pipati.length; i++) {
+    if (personajesJugador === pipati[i].nombre) {
+      return pipati[i];
+    }
+  }
+}
+
+function revisarColision(enemigo) {
+  const arribaEnemigo = enemigo.y;
+  const abajoEnemigo = enemigo.y + enemigo.alto;
+  const derechaEnemigo = enemigo.x + enemigo.ancho;
+  const izquierdaEnemigo = enemigo.x;
+
+  const arribaJugador = miJugador.y;
+  const abajoJugador = miJugador.y + miJugador.alto;
+  const derechaJugador = miJugador.x + miJugador.ancho;
+  const izquierdaJugador = miJugador.x;
+
+  if (
+    abajoJugador < arribaEnemigo ||
+    arribaJugador > abajoEnemigo ||
+    derechaJugador < izquierdaEnemigo ||
+    izquierdaJugador > derechaEnemigo
+  ) {
+    return;
+  }
+
+  detenerMovimiento();
+  clearInterval(intervalo);
+  sectionSeleccionarAtaque.style.display = "flex";
+  sectionVerMapa.style.display = "none";
+  seleccionarPersonajeEnemigo(enemigo);
 }
